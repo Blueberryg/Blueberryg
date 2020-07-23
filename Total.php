0348f059786b637e5e1b984037ef05b58c25f987
Blueberryg <67917884+Blueberryg@users.noreply.github.com>
2020-07-23 05:31:05 +0300

    Add v.1 files
    v.1 test

---
+++ Total.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Результаты</title>
+    </head>
+    <body>
+        <?php
+        $tot = ["Камиль","Адыл","Ринат"];
+        shuffle($tot);
+        print_r($tot);
+        ?>
+    </body>
+</html>
